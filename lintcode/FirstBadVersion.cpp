09f1e1705b6f4439b0c227fdb124e6578999ff93
cxh <sdqxcxh@gmail.com>
2015-04-11 22:26:20 +0800

    modify 20150411

--- lintcode/FirstBadVersion.cpp
+++ lintcode/FirstBadVersion.cpp
@@ -20,12 +20,56 @@ Do not call isBadVersion exceed O(logn) times.
 */
 
 #include "common.h"
+#include <stdlib.h>
+#include <time.h>
+class VersionControl {
+  public:
+    static int firstBadVersion;
+    static bool isBadVersion(int k) {
+      return k >= firstBadVersion;
+    }
+    static void init(int n) {
+        srand(time(NULL));
+        firstBadVersion = rand() % n + 1;
+        //cout << firstBadVersion <<endl;
+    }
+};
+int VersionControl::firstBadVersion = 0;
 
 class Solution {
 public:
-
+    /**
+     * @param n: An integers.
+     * @return: An integer which is the first bad version.
+     */
+    int findFirstBadVersion(int n) {
+      int ret = 0;
+      if (n >= 1) {
+        int beg = 1;
+        int end = n;
+        while (beg + 1 < end) {
+          int mid = beg + (end - beg) / 2;
+          if (VersionControl::isBadVersion(mid)) {
+            end = mid;
+          } else {
+            beg = mid;
+          }
+        }
+        if (VersionControl::isBadVersion(beg)) {
+          ret = beg;
+        } else if (VersionControl::isBadVersion(end)) {
+          ret = end;
+        }
+      }
+      return ret;
+    }
 };
 Solution s;
 
+
 TEST(FirstBadVersion, normal) {
+  for (int i = 1; i < 1000; i ++) {
+    VersionControl::init(i);
+    EXPECT_EQ(VersionControl::firstBadVersion, s.findFirstBadVersion(i));
+  }
 }
