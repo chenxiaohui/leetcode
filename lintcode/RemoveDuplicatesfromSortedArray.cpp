94a7f4ce4341cc695c76fa0ce070801fc5a8954a
cxh <sdqxcxh@gmail.com>
2015-04-13 23:02:03 +0800

    modify 20150413

--- lintcode/RemoveDuplicatesfromSortedArray.cpp
+++ lintcode/RemoveDuplicatesfromSortedArray.cpp
@@ -9,9 +9,48 @@ Example
 
 class Solution {
 public:
-
+    /**
+     * @param A: a list of integers
+     * @return : return an integer
+     */
+    int removeDuplicates(vector<int> &nums) {
+      int ret = 0;
+      if (nums.size() >= 1) {
+        int i = 0;
+        int j = i + 1;
+        while (true) {
+          while (j < static_cast<int>(nums.size()) && nums[j] == nums[i]) j++;
+          if (j < static_cast<int>(nums.size())) {
+            nums[++i] = nums[j++];
+          } else {
+            break;
+          }
+        }
+        ret = i + 1;
+      }
+      return ret;
+    }
 };
 Solution s;
 
 TEST(RemoveDuplicatesfromSortedArray, normal) {
+  vector<int> nums = {1,1,1,1,2,2,2,3,3};
+  EXPECT_EQ(3, s.removeDuplicates(nums));
+}
+TEST(RemoveDuplicatesfromSortedArray, empty)
+{
+  vector<int> nums;
+  EXPECT_EQ(0, s.removeDuplicates(nums));
+  nums.push_back(1);
+  EXPECT_EQ(1, s.removeDuplicates(nums));
+}
+TEST(RemoveDuplicatesfromSortedArray, allSame)
+{
+  vector<int> nums = {1,1,1,1};
+  EXPECT_EQ(1, s.removeDuplicates(nums));
+}
+TEST(RemoveDuplicatesfromSortedArray, allDifferent)
+{
+  vector<int> nums = {1,2,3,4};
+  EXPECT_EQ(4, s.removeDuplicates(nums));
 }
