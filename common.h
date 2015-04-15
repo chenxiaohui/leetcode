dba39cdbe726ce145e024e111578fbaa881a0755
cxh <sdqxcxh@gmail.com>
2015-04-15 23:57:38 +0800

    modify 20150415

--- common.h
+++ common.h
@@ -2,19 +2,40 @@
 #include <iterator>
 #include <algorithm>
 #include <stack>
+#include <string>
 #include <gtest/gtest.h>
- 
+
 using namespace std;
+
 #define n(A) sizeof(A)/sizeof(int)
 
-void print(const vector<int>& ve){
+template <typename T>
+bool equal(const vector<T>& A, const vector<T>& B) {
+  bool ret = false;
+  if (A.size() == B.size()) {
+    size_t na = A.size();
+    if (na == 0) {
+      ret = true;
+    } else {
+      size_t i = 0;
+      while (A[i] == B[i]) {
+        i++;
+      }
+      if (i == na) {
+        ret = true;
+      }
+    }
+  }
+  return ret;
+}
+
+template<typename T>
+void print(const vector<T>& ve){
    cout << "[";
-   copy(ve.begin(), ve.end(), ostream_iterator<int>(cout, ","));
-   //for (vector<int>::const_iterator it = ve.begin(); it != ve.end(); it++){
-     //cout << *it << ",";
-   //}
+   copy(ve.begin(), ve.end(), ostream_iterator<T>(cout, ","));
    cout <<"]" <<endl;
 }
+
 struct TreeNode {
     int val;
     TreeNode *left;
--- lintcode/MergeSortedArray.cpp
+++ lintcode/MergeSortedArray.cpp
@@ -17,9 +17,57 @@ How can you optimize your algorithm if one array is very large and the other is 
 
 class Solution {
 public:
-
+    /**
+     * @param A and B: sorted integer array A and B.
+     * @return: A new sorted integer array
+     */
+    vector<int> mergeSortedArray(vector<int> &A, vector<int> &B) {
+      vector<int> result;
+      int i = 0;
+      int j = 0;
+      int na = static_cast<int>(A.size());
+      int nb = static_cast<int>(B.size());
+      if (na > 0 && nb > 0) {
+        while (i < na && j < nb) {
+          if (A[i] < B[j]) {
+            result.push_back(A[i]);
+            i++;
+          } else {
+            result.push_back(B[j]);
+            j++;
+          }
+        }
+      }
+      if (i < na) {
+        while (i < na) {
+          result.push_back(A[i]);
+          i++;
+        }
+      } else if (j < nb) {
+        while (j < nb) {
+          result.push_back(B[j]);
+          j++;
+        }
+      }
+      return result;
+    }
 };
 Solution s;
 
 TEST(MergeSortedArray, normal) {
+  vector<int> A = {1, 2, 3, 4};
+  vector<int> B = {2, 4, 5, 6};
+  vector<int> exp = {1,2,2,3,4,4,5,6};
+  EXPECT_TRUE(equal(exp, s.mergeSortedArray(A, B)));
+}
+TEST(MergeSortedArray, empty) {
+  vector<int> A;
+  vector<int> B;
+  vector<int> C;
+  EXPECT_TRUE(equal(C, s.mergeSortedArray(A, B)));
+}
+TEST(MergeSortedArray, singleEmpty) {
+  vector<int> A = {1, 2, 3, 4};
+  vector<int> B;
+  EXPECT_TRUE(equal(A, s.mergeSortedArray(A, B)));
 }
