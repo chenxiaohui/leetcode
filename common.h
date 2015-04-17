0b29ff1fdd567375863240fdcce95c6a33f3afb6
cxh <sdqxcxh@gmail.com>
2015-04-17 23:23:30 +0800

    modify 20150417

--- common.h
+++ common.h
@@ -18,7 +18,7 @@ bool equal(const vector<T>& A, const vector<T>& B) {
       ret = true;
     } else {
       size_t i = 0;
-      while (A[i] == B[i]) {
+      while (A[i] == B[i] && i < na) {
         i++;
       }
       if (i == na) {
@@ -29,6 +29,24 @@ bool equal(const vector<T>& A, const vector<T>& B) {
   return ret;
 }
 
+template <typename T>
+bool equal(const T* A, int na, const T* B, int nb) {
+  bool ret = false;
+  if (na == nb) {
+    if (na == 0) {
+      ret = true;
+    } else {
+      int i = 0;
+      while (A[i] == B[i] && i < na) {
+        i++;
+      }
+      if (i == na) {
+        ret = true;
+      }
+    }
+  }
+  return ret;
+}
 template<typename T>
 void print(const vector<T>& ve){
    cout << "[";
--- lintcode/MergeSortedArrayII.cpp
+++ lintcode/MergeSortedArrayII.cpp
@@ -15,9 +15,60 @@ After merge, A will be filled as [1,2,3,4,5]
 
 class Solution {
 public:
-
+    /**
+     * @param A: sorted integer array A which has m elements, 
+     *           but size of A is m+n
+     * @param B: sorted integer array B which has n elements
+     * @return: void
+     */
+    void mergeSortedArray(int A[], int m, int B[], int n) {
+      int i = m - 1;
+      int j = n - 1;
+      int k = m + n - 1;
+      if (m > 0 & n > 0) {
+        while (i >= 0 && j >= 0) {
+          if (A[i] > B[j]) {
+            A[k--] = A[i--];
+          } else {
+            A[k--] = B[j--];
+          }
+        }
+      }
+      while (i >= 0) {
+        A[k--] = A[i--];
+      }
+      while (j >= 0) {
+        A[k--] = B[j--];
+      }
+    }
 };
 Solution s;
 
 TEST(MergeSortedArrayII, normal) {
+  int A[] = {1,2,3,0,0};
+  int B[] = {4,5};
+  s.mergeSortedArray(A, 3, B, n(B));
+  int C[] = {1,2,3,4,5};
+  EXPECT_TRUE(equal(A, 5, C, 5));
+}
+TEST(MergeSortedArrayII, mixed) {
+  int A[] = {1,2,3,4,5,0,0};
+  int B[] = {2,4};
+  s.mergeSortedArray(A, 5, B, n(B));
+  int C[] = {1,2,2,3,4,4,5};
+  EXPECT_TRUE(equal(A, 7, C, 7));
+}
+TEST(MergeSortedArrayII, AEmpty) {
+  int A[] = {0,0};
+  int B[] = {1,2};
+  s.mergeSortedArray(A, 0, B, n(B));
+  int C[] = {1,2};
+  EXPECT_TRUE(equal(A, 2, C, 2));
+}
+TEST(MergeSortedArrayII, BEmpty) {
+  int A[] = {};
+  int B[] = {};
+  s.mergeSortedArray(A, 0, B, n(B));
+  int C[] = {};
+  EXPECT_TRUE(equal(A, 0, C, 0));
 }
